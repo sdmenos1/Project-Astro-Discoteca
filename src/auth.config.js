445f4f042079cc7a3c2c004547d56740e5fe08bd
sdmenos1 <adr14nm4_30_12@hotmail.com>
2025-04-27 18:14:16 -0500

    cambios

--- src/auth.config.js
+++ src/auth.config.js
@@ -1,12 +1,11 @@
-// auth.config.ts
-import Facebook from '@auth/core/providers/facebook'
-import { defineConfig } from 'auth-astro'
+import Facebook from '@auth/core/providers/facebook';
+import { defineConfig } from 'auth-astro';
 
 export default defineConfig({
-	providers: [
-		Facebook({
-			clientId: import.meta.env.FACEBOOK_CLIENT_ID,
-			clientSecret: import.meta.env.FACEBOOK_CLIENT_SECRET,
-		}),
-	],
-})
+  providers: [
+    Facebook({
+      clientId: import.meta.env.FACEBOOK_CLIENT_ID,
+      clientSecret: import.meta.env.FACEBOOK_CLIENT_SECRET,
+    }),
+  ],
+});
